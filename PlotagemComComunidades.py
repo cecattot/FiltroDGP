ec5e08b1e685d6b3833177c17de677ffadf57329
cecattot <raulcctt@outlook.com>
2024-03-01 23:04:03 -0400

    Plot após considerações 01/03

--- PlotagemComComunidades.py
+++ PlotagemComComunidades.py
@@ -12,7 +12,7 @@ dfGrupos = pd.read_csv('dados_grupos.csv')
 # Mapeamento de cores para cada tipo de pesquisador
 cores = {
     "Pesquisador": "blue",
-    "TAE": "pink",
+    "TAE": "red",
     "Estudante": "purple",
     "Colaborador Estrangeiro": "khaki"
 }
@@ -56,23 +56,31 @@ for index, row in dfGrupos.iterrows():
 
 
 # Definir cores aleatórias para os demais campi
+# Lista de cores disponíveis em hexadecimal
+cores_disponiveis = ["#FF5733", "#FFBD33", "#FF3385", "#33FF57", "#33B0FF",
+                     "#B033FF", "#33FFBD", "#FF33E9", "#33FFA8", "#A833FF",
+                     "#FF336A", "#33FFC5", "#FFC533", "#3385FF", "#FFE933",
+                     "#33FFD8", "#FF33A8", "#33FF71", "#FF33C5"]
+
+# Mapeamento de cores para as chaves
 cores_comunidades = {
-    "Campus Barra do Garças": "#{:02x}{:02x}{:02x}".format(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)),
-    "Campus Cáceres": "#{:02x}{:02x}{:02x}".format(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)),
-    "Campus Campo Novo do Parecis": "#{:02x}{:02x}{:02x}".format(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)),
-    "Campus Cuiabá": "#{:02x}{:02x}{:02x}".format(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)),
-    "Campus Juína": "#{:02x}{:02x}{:02x}".format(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)),
-    "Campus Rondonópolis": "#{:02x}{:02x}{:02x}".format(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)),
-    "Campus São Vicente": "#{:02x}{:02x}{:02x}".format(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)),
-    "IFMT Campus Alta Floresta": "#{:02x}{:02x}{:02x}".format(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)),
-    "IFMT Campus Várzea Grande": "#{:02x}{:02x}{:02x}".format(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)),
-    "INSTITUTO FEDERAL DE EDUCAÇÃO, CIÊNCIA E TECNOLOGIA DE MATO GROSSO": "#{:02x}{:02x}{:02x}".format(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)),
-    "Instituto Federal de Mato Grosso - Campus Bela Vista": "#{:02x}{:02x}{:02x}".format(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)),
-    "Instituto Federal de Mato Grosso - Campus Confresa": "#{:02x}{:02x}{:02x}".format(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)),
-    "Instituto Federal de Mato Grosso - Campus Pontes e Lacerda": "#{:02x}{:02x}{:02x}".format(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)),
-    "Instituto Federal de Mato Grosso - Campus Primavera do Leste": "#{:02x}{:02x}{:02x}".format(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)),
-    "Instituto Federal de Mato Grosso - Campus Sorriso": "#{:02x}{:02x}{:02x}".format(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)),
-    "Reitoria": "#{:02x}{:02x}{:02x}".format(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    "BAG": cores_disponiveis[0],
+    "CAS": cores_disponiveis[1],
+    "CNP": cores_disponiveis[2],
+    "CBA": cores_disponiveis[3],
+    "JNA": cores_disponiveis[4],
+    "LRV": cores_disponiveis[5],
+    "ROO": cores_disponiveis[6],
+    "SVC": cores_disponiveis[7],
+    "ALF": cores_disponiveis[8],
+    "VGD": cores_disponiveis[9],
+    "GTA": cores_disponiveis[10],
+    "BLV": cores_disponiveis[11],
+    "CFS": cores_disponiveis[12],
+    "PLC": cores_disponiveis[13],
+    "PDL": cores_disponiveis[14],
+    "SRS": cores_disponiveis[15],
+    "RTR": cores_disponiveis[16]
 }
 
 # Definir cor dos vértices de acordo com a comunidade
@@ -99,19 +107,23 @@ visual_style = {
 plot(comunidades, "rede_com_comunidades.pdf", **visual_style)
 
 grafo.get_vertex_dataframe().to_csv('grafo_vertices.csv')
+grafo.get_edge_dataframe().to_csv('grafo_arestas.csv')
 
 # Criar um dicionário para mapear os índices dos vértices para os IDs dos grupos
 indice_para_grupo = {i: vertice['name'] for i, vertice in enumerate(grafo.vs)}
 
-# Criar um DataFrame para armazenar os resultados das comunidades
-df_comunidades = pd.DataFrame(columns=['Comunidade', 'ID do Grupo', 'Cor da Comunidade'])
+# Create an empty list to store community data
+communities_data = []
 
-# Preencher o DataFrame com os resultados das comunidades
-for i, comunidade in enumerate(comunidades):
-    cor_comunidade = visual_style['vertex_color'][comunidade[0]]
-    for indice_vertice in comunidade:
+# Populate the list with community data
+for i, community in enumerate(comunidades):
+    cor_comunidade = visual_style['vertex_color'][community[0]]
+    for indice_vertice in community:
         id_grupo = indice_para_grupo[indice_vertice]
-        df_comunidades = df_comunidades.append({'Comunidade': i, 'ID do Grupo': id_grupo, 'Cor da Comunidade': cor_comunidade}, ignore_index=True)
+        communities_data.append({'Comunidade': i, 'ID do Grupo': id_grupo, 'Cor da Comunidade': cor_comunidade})
 
-# Salvar o DataFrame em um arquivo CSV
+# Convert the list of dictionaries into a DataFrame
+df_comunidades = pd.DataFrame(communities_data)
+
+# Save the DataFrame to a CSV file
 df_comunidades.to_csv('comunidades.csv', index=False)
